f91ac955e0c6f9a7dde3a79557bab80657593df2
BrunoCosta-P <brunocosta524@hotmail.com>
2021-03-26 12:20:16 -0300

    Primeiro commit

---
+++ Aula_1/main.js
@@ -0,0 +1,15 @@
+/*var nome = "Bruno"
+var idade = 31
+
+var nomes = prompt("Digite seu nome")
+var idades = prompt("Digite sua idade")
+
+alert ("Olá "+nomes+" Sua idade é de "+idades+" anos")
+*/
+
+var dolar = 5.64
+
+var objeto = prompt("O que você deseja comprar?")
+var valorEmDolar = prompt("qual o vaor em dolar")
+var valorParaConverter = parseFloat(valorEmDolar)
+alert ("O valor do "+objeto+" em Real é de "+valorParaConverter*dolar)
---
+++ Aula_2/main.js
@@ -0,0 +1,26 @@
+var operacao = prompt("qual operação você deseja realizar agora? 1=soma 2=subtração 3=divisão 4=multiplicação")
+var primeiroValor = parseFloat( prompt("Dgigite o primeiro valor: "))
+var segundoValor = parseFloat(prompt('Digite o segundo valor:'))
+
+
+if (operacao == 1){
+    var resultado = primeiroValor+segundoValor
+    document.write("<h2>"+ primeiroValor + " + " + segundoValor+" = "+resultado+ "</h2>")
+}
+
+if (operacao == 2){
+    var resultado = primeiroValor-segundoValor
+    document.write("<h2>"+ primeiroValor + " - " + segundoValor+" = "+resultado+ "</h2>")
+}
+
+if (operacao == 3){
+    var resultado = primeiroValor/segundoValor
+    document.write("<h2>"+ primeiroValor + " / " + segundoValor+" = "+resultado+ "</h2>")
+}
+
+if (operacao == 4){
+    var resultado = primeiroValor*segundoValor
+    document.write("<h2>"+ primeiroValor + " X " + segundoValor+" = "+resultado+ "</h2>")
+}else {
+    document.write("<h2>opção invalida</h2>")
+}
