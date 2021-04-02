731a29d86e5ed36a53fecbf8086182d0ecdae5d2
BrunoCosta-P <brunocosta524@hotmail.com>
2021-04-02 13:44:17 -0300

    Add aula 6

---
+++ Aula_6/main.js
@@ -0,0 +1,65 @@
+var paulo = {nome: "Paulo", vitorias: 2, empates: 5, derrotas: 1, pontos:0}
+
+var rafa = {nome: "Rafa", vitorias: 3, empates: 5, derrotas: 2, pontos:0}
+
+var jogadores = [rafa, paulo]
+
+rafa.pontos = calculaPontos (rafa)
+paulo.pontos = calculaPontos(paulo)
+
+exibirJogadoresNaTela (jogadores)
+
+
+function calculaPontos(jogador){
+    var pontos = (jogador.vitorias * 3) + jogador.empates
+    return pontos
+}
+
+function exibirJogadoresNaTela(jogadores){
+    var html = ""
+    for(var i=0;i<jogadores.length;i++){
+        html += "<tr><td>"+jogadores[i].nome+"</td>"
+        html += "<td>"+jogadores[i].vitorias+"</td>"
+        html += "<td>"+jogadores[i].empates+"</td>"
+        html += "<td>"+jogadores[i].derrotas+"</td>"
+        html += "<td>"+jogadores[i].pontos+"</td>"
+        html += "<td><button onClick='adicionarVitoria("+i+")'>Vitória</button></td>"
+        html += "<td><button onClick='adicionarEmpate("+i+")'>Empate</button></td>"
+        html += "<td><button onClick='adicionarDerrota("+i+")'>Derrota</button></td></tr>"
+    }
+    var tabelaJogadores = document.getElementById("tabelaJogadores")
+    tabelaJogadores.innerHTML = html
+
+}
+
+
+function adicionarVitoria(i){
+    var jogador = jogadores[i]
+    jogador.vitorias++
+    jogador.pontos = calculaPontos(jogador)
+    exibirJogadoresNaTela(jogadores)
+}
+
+function adicionarEmpate(i){
+    var jogador = jogadores[i]
+    jogador.empates++
+    jogador.pontos = calculaPontos(jogador)
+    exibirJogadoresNaTela(jogadores)
+
+}
+
+function adicionarDerrota(i){
+    var jogador = jogadores[i]
+    jogador.derrotas++
+    exibirJogadoresNaTela(jogadores)
+}
+
+
+function adicionarJogador(){
+    var novoJogador = document.querySelector('#addPlayer').value
+    var player = {nome: novoJogador, vitorias: 0, empates: 0, derrotas: 0, pontos:0}
+    jogadores.push (player)
+    player.pontos = calculaPontos(player)
+    console.log(jogadores)
+    exibirJogadoresNaTela (jogadores)
+}
